f76dca6d4261cfee5b649df4b621cda72c2adb1a
Nasmer Fontanilla <nasmerfontanilla@gmail.com>
2025-04-24 23:17:36 +0800

    changed to backtick from apos in Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,7 @@ function Home() {
     // Run this once when page loads
     const fetchTrips = async () => {
       try {
-        const response = await axios.get("${BASE_URL}/api/trips");
+        const response = await axios.get(`${BASE_URL}/api/trips`);
         setTrips(response.data); // store trips in state
       } catch (error) {
         console.error("Error fetching trips:", error);
